208b6425ae4681f0c3699b81a2d3298d8d40b98e
hardza1230 <55472515+hardza1230@users.noreply.github.com>
2026-03-03 21:58:20 +0700

    03/03/2026 21:58

--- js/state.js
+++ js/state.js
@@ -59,5 +59,6 @@ export const dataStore = {
     charts: {}
 };
 
-export const GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTfJvJCGJfmarGs9LqNQfjozHgcdUUTHryMsim9soay_UF6UOcyB4KpIhwHwNvxaUqC5W1OpbIHjsH9/pub?gid=0&single=true&output=csv";
+export const MASTER_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTfJvJCGJfmarGs9LqNQfjozHgcdUUTHryMsim9soay_UF6UOcyB4KpIhwHwNvxaUqC5W1OpbIHjsH9/pub?gid=0&single=true&output=csv";
+export const CURRENT_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTfJvJCGJfmarGs9LqNQfjozHgcdUUTHryMsim9soay_UF6UOcyB4KpIhwHwNvxaUqC5W1OpbIHjsH9/pub?gid=0&single=true&output=csv"; // [แก้ไขตรงนี้] เปลี่ยน GID ให้เป็นเลขของแผ่นงาน Current
 export const APPS_SCRIPT_API_URL = "https://script.google.com/macros/s/AKfycbzvh06DciAs8HYw_N_i7_oaBgNjAp10gVZjGwNlt1TRzpd7OedEi3FgxJHCS41xdtVXMA/exec";
